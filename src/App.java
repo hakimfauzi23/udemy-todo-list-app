dcb14aa595ff9a2394440517c189c903ad179f41
Hanif <hanif.hakim@spesolution.com>
2022-12-26 08:59:03 +0700

    Create Important Method

--- src/App.java
+++ src/App.java
@@ -6,4 +6,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    /*
+     * Show ToDo List
+     */
+    public static void showTodoList() {
+
+    }
+
+    /*
+     * Add ToDo List
+     */
+    public static void addTodoList() {
+
+    }
+
+    /*
+     * Remove ToDo List
+     */
+    public static void removeTodoList() {
+
+    }
 }
