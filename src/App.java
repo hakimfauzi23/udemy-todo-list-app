f232d2823531c29eb294313b462229ce9d94bf80
Hanif <hanif.hakim@spesolution.com>
2022-12-26 08:57:03 +0700

    Create To Do List Model and Delete App. Bin

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
 public class App {
+
+    /* Create Model For To Do List Data */
+    public static String[] model = new String[10];
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
